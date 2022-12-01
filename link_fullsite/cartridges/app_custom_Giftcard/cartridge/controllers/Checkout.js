235b9cb8c88902dce55a282abe9b479078aa52d2
Gajendra Dubey <gajendra.dubey@codesquaretech.com>
2022-12-01 17:55:01 +0530

    giftcard initial

--- link_fullsite/cartridges/app_custom_Giftcard/cartridge/controllers/Checkout.js
+++ link_fullsite/cartridges/app_custom_Giftcard/cartridge/controllers/Checkout.js
@@ -192,62 +192,34 @@ var LineItemCtnr = require('dw/order/LineItemCtnr');
 var PersistentObject = require('dw/object/PersistentObject');
 var BasketMgr = require('dw/order/BasketMgr');
 var Order = require('dw/order/Order');
-var Basket = BasketMgr.getCurrentBasket();
 var Money = require('dw/value/Money');   
+var Basket = BasketMgr.getCurrentBasket();
+var currencyCode= req.session.currency.currencyCode
+try {
+giftCertificateCodeDetail=GiftCertificateMgr.getGiftCertificateByCode(giftCertificateCode);
+var Money = Money(appliedAmount,currencyCode);
+Transaction.wrap(()=>{
+    giftPaymentInstrument=Basket.createGiftCertificatePaymentInstrument(giftCertificateCode,Money);
+    redeemGiftDetail=GiftCertificateMgr.redeemGiftCertificate(giftPaymentInstrument);
+})
     // var SitePreferences = require('dw/system/SitePreferences');
-     var bonusPercentOfTotal= dw.system.Site.current.preferences.custom.bonusPercentOfTotal
-    var LineItemCtnr = require('dw/order/LineItemCtnr');
-    var totalNetPrice = currentBasket.totalNetPrice.value;
     
-    var toPercent = totalNetPrice * (bonusPercentOfTotal / 100);
-
-    if (currentCustomer.profile.custom.userWallet < bPoint) {
         var data = {
-            appliedPoint:bPoint,
             msg: "Applied bonus cannot be greater than wallet bonus",
             success: false
         };
 
         res.json(data);
-        return next();
-    }
-    if (Number(bPoint) > toPercent) {
-        var data = {
-            appliedPoint:bPoint,
-            msg: "Applied Bonus Point cannot be greater the "+ bonusPercentOfTotal+ " % of total",
-            success: false
-        };
-        res.json(data)
-        return next();
-    } 
-
-    try {
-        Transaction.wrap(function () {
-                var PriceAdjustment = currentBasket.createPriceAdjustment("bonusPointUses", new dw.campaign.AmountDiscount(bPoint));
-                currentCustomer.profile.custom.userWallet = currentCustomer.profile.custom.userWallet - Number(bPoint);
-                // var dPrice= LineItemCtnr.removePriceAdjustment(PriceAdjustment);
-
-                var nowPoint = currentCustomer.profile.custom.userWallet;
-                var data = {
-                    appliedPoint:bPoint,
-                    nowPoint: nowPoint,
-                    msg: "Bonus Point applied successfully",
-                    success: true
-                };
-                res.json(data)
-                // return next();
-        })
-
+       
     } catch (error) {
-        var promotionID = currentBasket.getPriceAdjustmentByPromotionID("bonusPointUses")
+        // var promotionID = currentBasket.getPriceAdjustmentByPromotionID("bonusPointUses")
         var data = {
-            appliedPoint:bPoint,
-            promotionID: JSON.stringify(promotionID),
+           error:error,
             msg: " Sorry, You have already applied bonus points",
             success: false
         };
         res.json(data)
-        return next();
+        
     }
 
     next();
