51e0a5dc8cfc069aee55f0970ad6376f8264285a
Gajendra Dubey <gajendra.dubey@codesquaretech.com>
2022-12-07 15:02:42 +0530

    new

--- link_fullsite/cartridges/app_custom_Giftcard/cartridge/controllers/Order.js
+++ link_fullsite/cartridges/app_custom_Giftcard/cartridge/controllers/Order.js
@@ -71,6 +71,7 @@ server.replace(
             order,
             { config: config, countryCode: currentLocale.country, containerView: 'order' }
         );
+        
         var passwordForm;
 
         var reportingURLs = reportingUrlsHelper.getOrderReportingURLs(order);
