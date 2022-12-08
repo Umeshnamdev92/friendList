0f91a3817bf48c7bfa0b14d0b466cf055aaf88fa
aashu89 <aashutosh.namdeo@codesquaretech.com>
2022-12-08 17:34:24 +0530

    aashu

--- link_fullsite/cartridges/app_custom_Giftcard/cartridge/client/default/js/Giftbtn.js
+++ link_fullsite/cartridges/app_custom_Giftcard/cartridge/client/default/js/Giftbtn.js
@@ -1,21 +1,27 @@
 $(document).ready(function () {
 
-    $("#sub").click(function () {
-        alert("ddd");
-        $.ajax({
-            url: "Order-newSubmit",
-            method: "POST",
-            data: $("#myform").serialize(),
-            success: function (resp) {
-             console.log(resp);
-             if(resp.success){
-              // alert("Your data sent Successfully");
-              document.getElementById("myform").reset();
-              $("#myModal").modal();
-             }
-            },
-          });
-      
-      });
+  $("#sub").click(function () {
+    alert("ddd");
+    $.ajax({
+      url: "Order-newSubmit",
+      method: "POST",
+      data: $("#myform").serialize(),
+      success: function (resp) {
+        console.log(resp);
+        if (resp.success) {
+          // alert("Your data sent Successfully");
+          document.getElementById("myform").reset();
+          $("#myModal").modal();
+        }
+      },
     });
-    
+
+  });
+
+  $(document).on('click' , '#other-amount' function(){
+    $('#other-input').show();
+  })
+
+});
+
+
