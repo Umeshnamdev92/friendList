bcdae2b91fa002a193afc0b3415a6e20755f88c2
saloni kael <saloni.kale@codesquaretech.com>
2023-01-11 15:52:06 +0530

    friend form

--- FriendListModule/cartridges/app_custom_friendlistmodule/cartridge/controllers/FriendListUpdated.js
+++ FriendListModule/cartridges/app_custom_friendlistmodule/cartridge/controllers/FriendListUpdated.js
@@ -8,8 +8,8 @@ var consentTracking = require("*/cartridge/scripts/middleware/consentTracking");
 var Transaction = require("dw/system/Transaction");
 var Logger = require("dw/system/Logger");
 var URLUtils = require("dw/web/URLUtils");
-var ProductList = require('dw/customer/ProductList');
-var ProductListMgr = require('dw/customer/ProductListMgr');
+var ProductList = require("dw/customer/ProductList");
+var ProductListMgr = require("dw/customer/ProductListMgr");
 
 server.get(
   "MahChild",
@@ -25,64 +25,62 @@ server.get(
 );
 
 server.post("Save", function (req, res, next) {
-  var form = server.forms.getForm("friendList");
-  var Transaction = require("dw/system/Transaction");
-  var Logger = require("dw/system/Logger");
-  var ProductListMgr = require("dw/customer/ProductListMgr");
-  var URLUtils = require("dw/web/URLUtils");
-  var new_Form = form.toObject();
+  var friendList = null;
+  var ListItem = null;
+  var data = res.getViewData();
+  var ListItemId = data.queryString.split("=")[1];
+  var friendForm = server.forms.getForm("friendList");
+  var formData = friendForm.toObject();
   var ProductMgr = require('dw/catalog/ProductMgr');
-  var product = ProductMgr.getProduct('FakeProduct');
-  var id = req.querystring.Id;
-    if(id != ''){
-        Transaction.wrap(function () {
-            var productLists = ProductListMgr.getProductLists(customer , 100);
-           
-            var productList = productLists[0];
-            
-            var productListt = productList.getItem(id);
-                productListt.custom.first_name = new_Form.firstName,
-                productListt.custom.last_name = new_Form.lastName,
-                productListt.custom.pet_name = new_Form.nickName,
-                productListt.custom.friend_birthday = new_Form.date,
-                productListt.custom.friend_phone = new_Form.phone     
-          });
-        
+
+
+  Transaction.wrap(function () {
+    var AllFriendList = ProductListMgr.getProductLists(customer, 100);
+    if (AllFriendList.length == 0) {
+      var newfriendList = ProductListMgr.createProductList(customer, 100);
+      friendList = newfriendList;
+      Logger.info(friendList);
+    } else {
+      friendList = AllFriendList[0];
+      Logger.info(friendList);
     }
 
-else{
     Transaction.wrap(function () {
-        var productList = ProductListMgr.getProductLists(customer , 100);
-        if(productList.length == 0){
-            var ProductList = ProductListMgr.createProductList(customer, 100)
-            productList = ProductList
-        }else
-        {
-            productList = productList[0];
-        }
+      if (ListItemId != "") {
+        var friendId = ListItemId;
+        ListItem = friendList.getItem(friendId);
+      } else {
+        var product = ProductMgr.getProduct("shampo");
+        ListItem = friendList.createProductItem(product);
+      }
+    });
 
-        var productList = productList.createProductItem(product);
-            productList.custom.first_name = new_Form.firstName,
-            productList.custom.last_name = new_Form.lastName,
-            productList.custom.pet_name = new_Form.nickName,
-            productList.custom.friend_birthday = new_Form.date,
-            productList.custom.friend_phone = new_Form.phone     
-      });
-    }
-  res.redirect(URLUtils.url("FriendListUpdated-FriendDataTable"));
+    ListItem.custom.first_name = formData.firstName,
+    ListItem.custom.last_name = formData.lastName,
+    ListItem.custom.friend_birthday = formData.date,
+    ListItem.custom.friend_phone = formData.phone,
+    ListItem.custom.address1 = formData.address1,
+    ListItem.custom.address2 = formData.address2,
+    ListItem.custom.country = formData.country,
+    ListItem.custom.city = formData.city,
+    ListItem.custom.states = formData.states.stateCode,
+    ListItem.custom.emailFriendList = formData.email,
+    ListItem.custom.zip = formData.zip;
+
+    res.redirect(URLUtils.url("FriendListUpdated-FriendDataTable"));
+  });
   next();
 });
 
 server.get("FriendDataTable", function (req, res, next) {
   var productListData = null;
   Transaction.wrap(function () {
-    var productList = ProductListMgr.getProductLists(customer , 100);
-    if(productList.length == 0){
-        var ProductList = ProductListMgr.createProductList(customer, 100)
-        productList = ProductList
-    }else
-    {
-        productList = productList[0];
+    var productList = ProductListMgr.getProductLists(customer, 100);
+    if (productList.length == 0) {
+      var ProductList = ProductListMgr.createProductList(customer, 100);
+      productList = ProductList;
+    } else {
+      productList = productList[0];
     }
     productListData = productList.getItems();
   });
@@ -93,13 +91,12 @@ server.get("FriendDataTable", function (req, res, next) {
 server.get("DeleteList", function (req, res, next) {
   var id = req.querystring.id;
   Transaction.wrap(function () {
-    var productList = ProductListMgr.getProductLists(customer , 100);
-    if(productList.length == 0){
-        var ProductList = ProductListMgr.createProductList(customer, 100)
-        productList = ProductList
-    }else
-    {
-        productList = productList[0];
+    var productList = ProductListMgr.getProductLists(customer, 100);
+    if (productList.length == 0) {
+      var ProductList = ProductListMgr.createProductList(customer, 100);
+      productList = ProductList;
+    } else {
+      productList = productList[0];
     }
     var reproductList = productList.getItem(id);
     var ad = productList.removeItem(reproductList);
@@ -108,30 +105,34 @@ server.get("DeleteList", function (req, res, next) {
   next();
 });
 
-server.get('EditList',function(req,res,next){
-    var id = req.querystring.id;
-    var form = server.forms.getForm("friendList");
-    var new_Form = {};
-    Transaction.wrap(function () {
-        var productList = ProductListMgr.getProductLists(customer , 100);
-        if(productList.length == 0){
-            var ProductList = ProductListMgr.createProductList(customer, 100)
-            productList = ProductList
-        }else
-        {
-            productList = productList[0];
-        }
-      var productList = productList.getItem(id);
-        new_Form.firstName= productList.custom.first_name,
-        new_Form.lastName= productList.custom.last_name,
-        new_Form.nickName= productList.custom.pet_name,
-        new_Form.date= productList.custom.friend_birthday,
-        new_Form.phone= productList.custom.friend_phone
+server.get("EditList", function (req, res, next) {
+  var id = req.querystring.id;
+  var form = server.forms.getForm("friendList");
+  var new_Form = {};
+  Transaction.wrap(function () {
+    var productList = ProductListMgr.getProductLists(customer, 100);
+    if (productList.length == 0) {
+      var ProductList = ProductListMgr.createProductList(customer, 100);
+      productList = ProductList;
+    } else {
+      productList = productList[0];
+    }
+    var productList = productList.getItem(id);
+    (new_Form.firstName = productList.custom.first_name),
+      (new_Form.lastName = productList.custom.last_name),
+      (new_Form.date = productList.custom.friend_birthday),
+      (new_Form.phone = productList.custom.friend_phone),
+      (new_Form.address1 = productList.custom.address1),
+      (new_Form.address2 = productList.custom.address2),
+      (new_Form.country = productList.custom.country),
+      (new_Form.city = productList.custom.city),
+      (new_Form.states = productList.custom.states),
+      (new_Form.email = productList.custom.emailFriendList),
+      (new_Form.zip = productList.custom.zip);
   });
   form.copyFrom(new_Form);
-  res.render('friendList/editFriendForm' , {form: form , Id: id});
+  res.render("friendList/editFriendForm", { form: form, Id: id });
   next();
-    
-})
+});
 
 module.exports = server.exports();
