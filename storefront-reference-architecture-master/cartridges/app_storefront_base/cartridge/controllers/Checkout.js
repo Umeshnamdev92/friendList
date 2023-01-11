f7283921ad671dfa3e656d3f51fdd2c580aa1121
Anmol Vyas <anmol.vyas@codesquaretech.com>
2023-01-11 15:55:23 +0530

    anmol

--- storefront-reference-architecture-master/cartridges/app_storefront_base/cartridge/controllers/Checkout.js
+++ storefront-reference-architecture-master/cartridges/app_storefront_base/cartridge/controllers/Checkout.js
@@ -156,7 +156,32 @@ server.get(
                 currentStage = 'shipping';
             }
         }
+//         var csrfProtection = require("*/cartridge/scripts/middleware/csrf");
+// var userLoggedIn = require("*/cartridge/scripts/middleware/userLoggedIn");
+// var consentTracking = require("*/cartridge/scripts/middleware/consentTracking");
+// var Transaction = require("dw/system/Transaction");
+// var Logger = require("dw/system/Logger");
+// var URLUtils = require("dw/web/URLUtils");
+// var ProductList = require('dw/customer/ProductList');
+// var ProductListMgr = require('dw/customer/ProductListMgr');
 
+//         var id = req.querystring.id;
+//         var productlist = [];
+//         Transaction.wrap(function () {
+//             var productList = ProductListMgr.getProductLists(customer , 100);
+//             if(productList.length == 0){
+//                 var ProductList = ProductListMgr.createProductList(customer, 100)
+//                 productList = ProductList
+//             }else
+//             {
+//                 productList = productList[0];
+//             }
+//             var productList = productList.getItem(id);
+//             productlist.push(productList);
+            
+//           });
+        
+        
         res.render('checkout/checkout', {
             order: orderModel,
             customer: accountModel,
