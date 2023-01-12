d2e18d6b7c0fdc4aedc482cdc0aeccdd20c19bf6
ISHIKA-CODESQUARE <ishika.joshi@codesquaretech.com>
2023-01-12 15:59:41 +0530

    modal merged

--- FriendListModule/cartridges/app_custom_friendlistmodule/cartridge/controllers/Checkout.js
+++ FriendListModule/cartridges/app_custom_friendlistmodule/cartridge/controllers/Checkout.js
@@ -1,6 +1,9 @@
 'use strict'
 var server = require("server");
 
+var page = require("app_storefront_base/cartridge/controllers/Checkout");
+server.extend(page);
+
 var csrfProtection = require("*/cartridge/scripts/middleware/csrf");
 var userLoggedIn = require("*/cartridge/scripts/middleware/userLoggedIn");
 var consentTracking = require("*/cartridge/scripts/middleware/consentTracking");
@@ -13,6 +16,8 @@ var ProductListMgr = require('dw/customer/ProductListMgr');
 server.append('Begin',function(req,res,next){
     var id = req.querystring.id;
     var productlist = [];
+    var productListData = null;
+
     Transaction.wrap(function () {
         var productList = ProductListMgr.getProductLists(customer , 100);
         if(productList.length == 0){
@@ -22,13 +27,16 @@ server.append('Begin',function(req,res,next){
         {
             productList = productList[0];
         }
+        productListData = productList.getItems();
+
         var productList = productList.getItem(id);
-        productlist.push(productList);   
+        productlist.push(productList);
       });
 
     res.render('checkout/checkout',{
         id:id,
-        productList : productlist
+        productList : productlist,
+        productList: productListData
     });
     
     next();
