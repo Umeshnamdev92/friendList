486a766d9ad39bea1776018fe6500ce829aa03b6
Anmol Vyas <anmol.vyas@codesquaretech.com>
2023-01-12 14:10:25 +0530

    merging with ishika

--- FriendListModule/cartridges/app_custom_Giftcard/cartridge/models/order.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-function OrderModel(lineItemContainer, options) {
-    module.superModule.call(this,lineItemContainer, options);
-       this.giftCardTotal = lineItemContainer.giftCertificateTotalPrice ? lineItemContainer.giftCertificateTotalPrice.value :null;
-}
-
-module.exports = OrderModel;
